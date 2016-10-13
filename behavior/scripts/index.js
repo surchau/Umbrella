2946de0b145a227d3fe53e3978187cb48b569a5a
surchau <sureechachauhan@yahoo.com>
2016-10-13 14:15:33 -0400

    Updated index for city name

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -1,4 +1,12 @@
 'use strict'
+const firstOfEntityRole = function (message, entity, role) {
+    role = role || 'generic';
+    const slots = message.slots
+    const entityValues = message.slots[entity]
+    const valsForRole = entityValues ? entityValues.values_by_role[role] : null
+    return valsForRole ? valsForRole[0]:null
+}
+
 
 exports.handle = function handle(client) {
 //Add collectCity and provideWeather
@@ -7,9 +15,20 @@ const collectCity=client.createStep({
 	satisfied(){
 	return Boolean(client.getConversationState().weatherCity)
 	},
+	extractInfo(){
+	    const city=firstOfEntityRole(client.getMessagePart(),'city')
+	    if(city){
+	        client.updateConversationState({
+                weatherCity:city,
+	        })
+            console.log('User wants the weather in: ',city.value)
+	    }
+	},
+
 	prompt(){
 	//Need to prompt user for city
-	console.log('Need to ask user for city')
+	    //console.log('Need to ask user for city')
+        client.addResponse('app:response:name:prompt/weather_city')
 	client.done()
 	},
 	})
