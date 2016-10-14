4eb6a1aebbc094ec3d10af9e2b7ad5b4ed858ee9
surchau <sureechachauhan@yahoo.com>
2016-10-13 20:28:35 -0400

    Updated openweather key

--- behavior/scripts/lib/getCurrentWeather.js
+++ behavior/scripts/lib/getCurrentWeather.js
@@ -4,9 +4,12 @@
 const request = require('request')
 
 module.exports = function getCurrentWeather(locationName, next) {
-    const appId = '9857cc55571348bcf9b8df75648e629f'
+    //const appId = '9857cc55571348bcf9b8df75648e629f'
+    const appId='243a4be2ddf76b9cf7299849d34103a8'
 
-    const requestUrl = `http://api.openweathermap.org/data/2.5/weather?units=imperial&appid=${appId}&q=${locationName}`
+    //const requestUrl = `http://api.openweathermap.org/data/2.5/weather?units=imperial&appid=${appId}&q=${locationName}'
+    const requestUrl = 'http://api.openweathermap.org/data/2.5/weather?units=imperial&appid=243a4be2ddf76b9cf7299849d34103a8&q=${locationName}'
+    
 
     console.log('Making HTTP GET request to:', requestUrl)
 
