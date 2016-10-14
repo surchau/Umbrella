a07aa95b6ff9860c2ee918416a440070e08d6783
surchau <sureechachauhan@yahoo.com>
2016-10-13 20:52:01 -0400

    Updated index and getCurrentWeather

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -1,135 +1,108 @@
 'use strict'
 
-const getCurrentWeather=require('./lib/getCurrentWeather')
+const getCurrentWeather = require('./lib/getCurrentWeather')
 
 const firstOfEntityRole = function (message, entity, role) {
     role = role || 'generic';
+
     const slots = message.slots
     const entityValues = message.slots[entity]
     const valsForRole = entityValues ? entityValues.values_by_role[role] : null
-    return valsForRole ? valsForRole[0]:null
+
+    return valsForRole ? valsForRole[0] : null
 }
 
-
 exports.handle = function handle(client) {
-//Add collectCity and provideWeather
+    const sayHello = client.createStep({
+        satisfied() {
+            return Boolean(client.getConversationState().helloSent)
+        },
 
-const collectCity=client.createStep({
-	satisfied(){
-	return Boolean(client.getConversationState().weatherCity)
-	},
-	extractInfo(){
-	    const city=firstOfEntityRole(client.getMessagePart(),'city')
-	    if(city){
-	        client.updateConversationState({
-                weatherCity:city,
-	        })
-            console.log('User wants the weather in: ',city.value)
-	    }
-	},
+        prompt() {
+            client.addResponse('app:response:name:welcome')
+            client.addResponse('app:response:name:provide/documentation', {
+                documentation_link: 'http://docs.init.ai',
+            })
+            client.addResponse('app:response:name:provide/instructions')
+            client.updateConversationState({
+                helloSent: true
+            })
+            client.done()
+        }
+    })
 
-	prompt(){
-	//Need to prompt user for city
-	    //console.log('Need to ask user for city')
-        client.addResponse('app:response:name:prompt/weather_city')
-	client.done()
-	},
-	})
+    const untrained = client.createStep({
+        satisfied() {
+            return false
+        },
 
-const provideWeather=client.createStep({
-	satisfied(){
-	return false
-	},
-	prompt(callback) {
-	    getCurrentWeather(client.getConversationState().weatherCity.value,resultBody =>{
-	        if(!resultBody || resultBody.cod !==200){
-	            console.log('Error getting weather.')
-	            callback()
-                return
-	        }
+        prompt() {
+            client.addResponse('app:response:name:apology/untrained')
+            client.done()
+        }
+    })
 
-	        const weatherDescription=(
-                resultBody.weather.length > 0 ?
-                resultBody.weather[0].description:
-                null
+    const collectCity = client.createStep({
+        satisfied() {
+            return Boolean(client.getConversationState().weatherCity)
+        },
+
+        extractInfo() {
+            const city = firstOfEntityRole(client.getMessagePart(), 'city')
+            if (city) {
+                client.updateConversationState({
+                    weatherCity: city,
+                })
+                console.log('User wants the weather in:', city.value)
+            }
+        },
+
+        prompt() {
+            client.addResponse('app:response:name:prompt/weather_city')
+            client.done()
+        },
+    })
+
+    const provideWeather = client.createStep({
+        satisfied() {
+            return false
+        },
+
+        prompt(callback) {
+            getCurrentWeather(client.getConversationState().weatherCity.value, resultBody => {
+                if (!resultBody || resultBody.cod !== 200) {
+                    console.log('Error getting weather.')
+                    callback()
+                    return
+                }
+
+                const weatherDescription = (
+                  resultBody.weather.length > 0 ?
+                  resultBody.weather[0].description :
+                  null
                 )
 
-	            const weatherData={
-	                temperature:result.main.temp,
-	                condition:weatherDescription,
-                    city:resultBody.name,
-	            }
-	            console.log('sending real weather:', weatherData)
-	            client.addResponse('app:response:name:provide_weather/current',weatherData)
+                const weatherData = {
+                    temperature: resultBody.main.temp,
+                    condition: weatherDescription,
+                    city: resultBody.name,
+                }
+
+                console.log('sending real weather:', weatherData)
+                client.addResponse('app:response:name:provide_weather/current', weatherData)
                 client.done()
+
                 callback()
+            })
+        },
+    })
 
-
-	    })
-	    //Need to provide weather
-	    let weatherData = {
-	        temperature: 60,
-	        condition: 'sunny',
-            city:client.getConversationState().weatherCity.value,
-	    }
-        client.addResponse('app:response:name:provide_weather/current',weatherData)
-	client.done()
-	},
-})
-
-
-
-  const sayHello = client.createStep({
-    satisfied() {
-      return Boolean(client.getConversationState().helloSent)
-    },
-
-    prompt() {
-      client.addResponse('app:response:name:welcome')
-      client.addResponse('app:response:name:provide/documentation', {
-        documentation_link: 'http://docs.init.ai',
-      })
-      client.addResponse('app:response:name:provide/instructions')
-      client.updateConversationState({
-        helloSent: true
-      })
-      client.done()
-    }
-  })
-
-  const untrained = client.createStep({
-    satisfied() {
-      return false
-    },
-
-    prompt() {
-      client.addResponse('app:response:name:apology/untrained')
-      client.done()
-    }
-  })
-
-  //client.runFlow({
-  //  classifications: {
-			
-  //  },
-    
-   // streams: {
-    //  main: 'onboarding',
-    //  onboarding: [sayHello],
-     // end: [untrained]
-  //  }
- // })
-//Add new value to the flow
-	client.runFlow({
-	classification:{},
-	streams:{
-		main: 'getWeather',
-		hi:[sayHello],
-		getWeather:[collectCity,provideWeather],
-	}
-	})
-
-
-
-
+    client.runFlow({
+        classifications: {},
+        streams: {
+            main: 'getWeather',
+            hi: [sayHello],
+            getWeather: [collectCity, provideWeather],
+        }
+    })
 }
--- behavior/scripts/lib/getCurrentWeather.js
+++ behavior/scripts/lib/getCurrentWeather.js
@@ -1,16 +1,12 @@
-// JavaScript source code
 'use strict'
 
 const request = require('request')
 
 module.exports = function getCurrentWeather(locationName, next) {
-    //const appId = '9857cc55571348bcf9b8df75648e629f'
-    const appId='243a4be2ddf76b9cf7299849d34103a8'
+    const appId = '243a4be2ddf76b9cf7299849d34103a8'
 
-    //const requestUrl = `http://api.openweathermap.org/data/2.5/weather?units=imperial&appid=${appId}&q=${locationName}'
-    const requestUrl = 'http://api.openweathermap.org/data/2.5/weather?units=imperial&appid=243a4be2ddf76b9cf7299849d34103a8&q=Boston'
-    
-
+    const requestUrl = 'http://api.openweathermap.org/data/2.5/weather?units=imperial&appid=${appId}&q=${locationName}'
+        
     console.log('Making HTTP GET request to:', requestUrl)
 
     request(requestUrl, (err, res, body) => {
