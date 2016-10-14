74e024418fa19dad0e5a0107cfdc2eaec8f357a9
surchau <sureechachauhan@yahoo.com>
2016-10-13 21:39:51 -0400

    Updated getCurrentWeather 5

--- behavior/scripts/lib/getCurrentWeather.js
+++ behavior/scripts/lib/getCurrentWeather.js
@@ -8,6 +8,8 @@ module.exports = function getCurrentWeather(locationName, next) {
     //const requestUrl = 'http://api.openweathermap.org/data/2.5/weather?units=imperial&appid=${appId}&q=${locationName}'
     // const requestUrl = 'http://api.openweathermap.org/data/2.5/weather?units=imperial&appid=' + appId + '&q=' + { locationName }
     
+    console.log('locationName is ', locationName)
+
     const requestUrl='http://api.openweathermap.org/data/2.5/weather?units=imperial&appid=243a4be2ddf76b9cf7299849d34103a8&q='+locationName
     console.log('Making HTTP GET request to:', requestUrl)
 
